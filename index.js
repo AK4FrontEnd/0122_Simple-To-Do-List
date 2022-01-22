d2d188961f7092e2565f0d1d5307ebe6426e618f
Annie Kuo <ak4f2e@gmail.com>
2022-01-23 02:13:13 +0800

    fix: 同一天的待辦新增

--- index.js
+++ index.js
@@ -4,16 +4,31 @@ window.onload = function(){
     document.querySelector('#addToDoButton').addEventListener('click', function(){
         title = document.querySelector('.title-input');
         deadline = document.querySelector('.date-input');
-        postToDo(title.value, deadline.value);
+        isDateDataExist(deadline.value);
     })
 }
 
-function postToDo(titleStr, deadlineDate){
-    // 1. 防呆：判斷是否有資料未填寫
+function isDateDataExist(deadlineDate){
+    var obj = JSON.parse(localStorage.getItem(deadlineDate));
+    if(obj == null){
+        addToDoGroup(title.value, deadlineDate)
+    }else{
+        let item = {
+            title: title.value,
+            "finished": false
+        }
+        (obj.list).push(item);
+        localStorage.setItem(deadlineDate, JSON.stringify(obj));
+
+        title.value = '';
+        deadline.value = '';
+    }
+}
+
+function addToDoGroup(titleStr, deadlineDate){
     if(titleStr == '' || deadlineDate == ''){
         alert('請輸入標題與日期');
     }else{
-        // 2. 將輸入的待辦名稱與日期資料存成事先設計好的資料結構
         let obj = {
             "deadline": deadlineDate,
             "expired": false,
@@ -24,9 +39,8 @@ function postToDo(titleStr, deadlineDate){
                 }
             ]
         }
-        // 3. 存進 Local Storage
         localStorage.setItem(deadlineDate, JSON.stringify(obj));
-        // 4. 清空 input 資料
+
         title.value = '';
         deadline.value = '';
     }
